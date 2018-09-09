a6eb7a4bac050d51f00b7940d0b42883143467e7
Ben Duncan <ben@heypiggybank.com>
2018-09-09 12:28:08 +1000

    Initial commit for #govhack2018 entry

---
+++ README.md
@@ -0,0 +1,6 @@
+# EasyRider
+
+Traffic flow as a service
+
+Desgined by the cool cats from https://www.peregianhub.com.au/ for #govhack2018
+
---
+++ vehicle-recognition/config.js
@@ -0,0 +1,24 @@
+const buttons = [
+  {
+    url: "https://govhack2018.itmad.com.au/api/v1/sensor-data",
+    percent: 0.8,
+    data: {
+      id: 1,
+      sensor_id: 18,
+      direction: "in",
+      timestamp: 0
+    }
+  },
+  {
+    url: "https://govhack2018.itmad.com.au/api/v1/sensor-data",
+    percent: 0.8,
+    data: {
+      id: 1,
+      sensor_id: 18,
+      direction: "out",
+      timestamp: 0
+    }
+  }
+];
+
+export default buttons;
